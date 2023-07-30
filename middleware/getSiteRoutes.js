fac467b4a57548fd60c9e97419b8aef368f88649
Ionelll <serban.nelutu@gmail.com>
2023-07-30 08:17:14 +0200

    sixt commit

--- middleware/getSiteRoutes.js
+++ middleware/getSiteRoutes.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
 
-router.get("/", (req, res) => {
-	res.sendFile("index.html", { root: __dirname });
+router.get("*", (req, res) => {
+	res.render("index.html");
 });
 module.exports = router;
