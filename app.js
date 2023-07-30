682e2cc92eef0bc2ea860983e6f73bdf5ecadf94
Ionelll <serban.nelutu@gmail.com>
2023-07-30 08:54:31 +0200

    16 commit

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const getSite = require("./middleware/getSiteRoutes");
 app = express();
 
 app.use("/api", mailRoutes);
-app.use("", getSite);
+app.use("/", getSite);
 app.use(express.static("./views"));
 app.use(express.json());
 module.exports = app;
