067af415599d3c3a7fd181ecb86f39a6ea2b4426
Ionelll <serban.nelutu@gmail.com>
2023-07-30 08:58:51 +0200

    18 commit

--- middleware/getSiteRoutes.js
+++ middleware/getSiteRoutes.js
@@ -2,6 +2,6 @@ const express = require("express");
 const router = express.Router();
 
 router.get("/", (req, res) => {
-	res.sendFile("./views/index.html");
+	res.sendFile("./views/index.html", { root: __dirname });
 });
 module.exports = router;
